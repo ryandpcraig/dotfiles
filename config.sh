6d427f3df12535d7b18b6253ded5a4dbcb8293fc
Ryan Craig <ryan.craig@smartbox.com>
2017-01-19 10:46:06 +0000

    Updated personal variables etc

--- config.sh
+++ config.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-NAME='Luis Davim'
-EMAIL='luis.davim@smartbox.com'
-
-WORKINGCOPIES='$HOME/workingCopies'
-CODE='$HOME/workingCopies/code'
-GITHUB='$HOME/workingCopies/code/github'
-GITLAB='$HOME/workingCopies/code/gitlab'
-DOCKERFILES='$HOME/workingCopies/code/gitlab/dockerfiles'
-TERRAFORM='$HOME/workingCopies/code/gitlab/terraform'
-CHEF_ROLES='$HOME/workingCopies/code/gitlab/chef/roles'
-CHEF_ENVIRONMENTS='$HOME/workingCopies/code/gitlab/chef/environments'
-CHEF_HANDLERS='$HOME/workingCopies/code/gitlab/chef/handlers'
-CHEF_DATA_BAGS='$HOME/workingCopies/code/gitlab/chef-data-bags'
-CHEF_COOKBOOKS='$HOME/workingCopies/code/gitlab/chef-cookbooks'
+CHEF_COOKBOOKS="/home/ryancrai/gitlab/chef-cookbooks"
+GITLAB="/home/ryancrai/gitlab"
+CHEF_DATA_BAGS="/home/ryancrai/gitlab/chef-data-bags"
+DOCKERFILES="/home/ryancrai/gitlab/dockerfiles"
+NAME="Ryan Craig"
+CHEF_ROLES="/home/ryancrai/gitlab/chef/roles"
+GITHUB="/home/ryancrai/github"
+EMAIL="ryan.craig@smartbox.com"
+CHEF_ENVIRONMENTS="/home/ryancrai/gitlab/chef/environments"
